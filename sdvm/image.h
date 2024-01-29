330ce2dbf2b6f4c5bc1055ae3a5a7592a7325dd7
Ronie Salgado <roniesalg@gmail.com>
2024-01-28 22:44:23 -0300

    I added more section names.

--- sdvm/image.h
+++ sdvm/image.h
@@ -3,11 +3,20 @@
 
 #include <stdint.h>
 
+#define SDVM_IM_FOUR_CC(c1, c2, c3, c4) ((c1) | ((c2) << 8) | ((c3) << 16) | ((c4) << 24))
+
 typedef enum sdvm_imageSectionType_e
 {
     SdvmImageSectionTypeNull = 0,
-    SdvmImageSectionTypeConstant = 1,
-    SdvmImageSectionTypeText = 2,
+    SdvmImageSectionTypeConstant = SDVM_IM_FOUR_CC('c', 'o', 'n', 't'),
+    SdvmImageSectionTypeText = SDVM_IM_FOUR_CC('t', 'e', 'x', 't'),
+    SdvmImageSectionTypeFunctionTable = SDVM_IM_FOUR_CC('f', 'u', 'n', 't'),
+
+    SdvmImageSectionTypeExportTable = SDVM_IM_FOUR_CC('e', 'x', 'p', 't'),
+    SdvmImageSectionTypeImportTable = SDVM_IM_FOUR_CC('i', 'm', 'p', 't'),
+
+    SdvmImageSectionTypeDebugLineStart = SDVM_IM_FOUR_CC('d', 'l', 'n', 's'),
+    SdvmImageSectionTypeDebugLineEnd = SDVM_IM_FOUR_CC('d', 'l', 'n', 'e'),
 } sdvm_imageSectionType_t;
 
 typedef struct sdvm_imageHeader_s
