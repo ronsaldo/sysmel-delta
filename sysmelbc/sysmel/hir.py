001e048012f4a6d8b8d02bd1cf9ee3168e694ad2
Ronie Salgado <roniesalg@gmail.com>
2024-02-20 07:30:51 -0300

    HIR IR generation WIP.

--- sysmelbc/sysmel/hir.py
+++ sysmelbc/sysmel/hir.py
@@ -2,8 +2,35 @@ from abc import ABC, abstractmethod
 from .ghir import *
 
 class HIRContext:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, pointerSize = 8) -> None:
+        self.typeUniverses = dict()
+        self.pointerSize = pointerSize
+        self.pointerAlignment = pointerSize
+        self.gcPointerSize = pointerSize*2
+        self.gcPointerAlignment = pointerSize
+
+        self.anySize = self.gcPointerSize
+        self.anyAlignment = self.gcPointerAlignment
+        self.functionTypeSize = self.gcPointerSize
+        self.functionTypeAlignment = self.gcPointerAlignment
+        self.anyType = HIRAnyType(self, 'Any', self.anySize, self.anyAlignment)
+        self.booleanType = HIRPrimitiveBooleanType(self, 'Boolean', 1, 1)
+        self.unitType = HIRUnitType(self, 'Unit', 0, 1)
+        self.int8Type   = HIRPrimitiveIntegerType(self, 'Int8',   True, 1, 1)
+        self.int16Type  = HIRPrimitiveIntegerType(self, 'Int16',  True, 2, 2)
+        self.int32Type  = HIRPrimitiveIntegerType(self, 'Int32',  True, 4, 4)
+        self.int64Type  = HIRPrimitiveIntegerType(self, 'Int64',  True, 8, 8)
+        self.uint8Type  = HIRPrimitiveIntegerType(self, 'UInt8',  False, 1, 1)
+        self.uint16Type = HIRPrimitiveIntegerType(self, 'UInt16', False, 2, 2)
+        self.uint32Type = HIRPrimitiveIntegerType(self, 'UInt32', False, 4, 4)
+        self.uint64Type = HIRPrimitiveIntegerType(self, 'UInt64', False, 8, 8)
+        self.float32Type = HIRPrimitiveFloatType(self, 'Float32', 4, 4)
+        self.float64Type = HIRPrimitiveFloatType(self, 'Float64', 8, 8)
+
+    def getTypeUniverse(self, index):
+        if index not in self.typeUniverses:
+            self.typeUniverses[index] = HIRTypeUniverse(self, index)
+        return self.typeUniverses[index]
 
 class HIRValue(ABC):
     def __init__(self, context: HIRContext) -> None:
@@ -13,6 +40,99 @@ class HIRValue(ABC):
     def getType(self):
         pass
 
+class HIRTypeValue(HIRValue):
+    def  __init__(self, context: HIRContext) -> None:
+        super().__init__(context)
+
+    def isType(self):
+        return True
+
+    @abstractmethod
+    def getSize(self) -> int:
+        pass
+
+    @abstractmethod
+    def getAlignment(self) -> int:
+        pass
+
+class HIRTypeUniverse(HIRTypeValue):
+    def  __init__(self, context: HIRContext, index) -> None:
+        super().__init__(context)
+        self.index = index
+
+    def getType(self):
+        return self.context.getTypeUniverse(self.index + 1)
+
+    def getSize(self) -> int:
+        return self.context.anySize
+
+    def getAlignment(self) -> int:
+        return self.context.anyAlignment
+    
+    def __str__(self) -> str:
+        if self.index == 0:
+            return 'Type'
+        return 'Type@%d' % self.index
+
+class HIRPrimitiveType(HIRTypeValue):
+    def  __init__(self, context: HIRContext, name: str, size: int, alignment: int) -> None:
+        super().__init__(context)
+        self.name = name
+        self.size = size
+        self.alignment = alignment
+
+    def getAlignment(self) -> int:
+        return self.alignment
+    
+    def getSize(self) -> int:
+        return self.size
+
+    def getType(self):
+        return self.context.getTypeUniverse(0)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class HIRAnyType(HIRPrimitiveType):
+    pass
+
+class HIRUnitType(HIRPrimitiveType):
+    pass
+
+class HIRPrimitiveBooleanType(HIRPrimitiveType):
+    pass
+
+class HIRPrimitiveIntegerType(HIRPrimitiveType):
+    def __init__(self, context: HIRContext, name: str, isSigned: bool, size: int, alignment: int) -> None:
+        super().__init__(context, name, size, alignment)
+        self.isSigned = isSigned
+
+class HIRPrimitiveFloatType(HIRPrimitiveType):
+    pass
+
+class HIRFunctionType(HIRTypeValue):
+    def __init__(self, context: HIRContext) -> None:
+        super().__init__(context)
+        self.argumentTypes = []
+        self.resultType: HIRValue = None
+
+    def __str__(self) -> str:
+        result = '('
+        for arg in self.argumentTypes:
+            result += str(arg)
+        result += '-> '
+        result += str(self.resultType)
+        return result
+    
+    def getAlignment(self) -> int:
+        return self.context.functionTypeAlignment
+    
+    def getSize(self) -> int:
+        return self.context.functionTypeSize
+
+    def getType(self):
+        return self.context.getTypeUniverse(0)    
+
 class HIRConstant(HIRValue):
     def __init__(self, context: HIRContext, type: HIRValue) -> None:
         super().__init__(context)
@@ -21,14 +141,35 @@ class HIRConstant(HIRValue):
     def getType(self):
         return self.type
 
-class HIRFunctionalDefinition(HIRConstant):
+class HIRGlobalValue(HIRConstant):
+    def __init__(self, context: HIRContext, type: HIRValue) -> None:
+        super().__init__(context, type)
+        self.name = None
+        self.globalIndex = 0
+
+    def __str__(self) -> str:
+        if self.name is not None:
+            return '@%s|%d' % (self.name, self.globalIndex)
+        return '@%d' % self.globalIndex
+    
+    def fullPrintString(self) -> str:
+        return str(self)
+
+class HIRFunctionalDefinition(HIRGlobalValue):
     def __init__(self, context: HIRContext, type: HIRValue) -> None:
         super().__init__(context, type)
         self.firstBasicBlock: HIRBasicBlock = None
         self.lastBasicBlock: HIRBasicBlock = None
 
     def fullPrintString(self) -> str:
-        result = ''
+        result = str(self)
+        result += ' := ['
+        result += ']'
+        result += '('
+        result += ') : '
+        result += str(self.type)
+        result += ' {\n'
+        result += '}\n'
         return result
 
 class HIRFunctionalLocalValue(HIRValue):
@@ -46,22 +187,48 @@ class HIRConstantLambda(HIRConstant):
         self.captures = captures
         self.definition = definition
 
+    def __str__(self) -> str:
+        result = 'lambda '
+        result += str(self.definition)
+        result += ' captures ['
+        isFirst = True
+        for capture in self.captures:
+            if isFirst:
+                isFirst = False
+            else:
+                result += ', '
+            result += str(capture)
+        result += '] : '
+        result += str(self.type)
+        return result
+
 class HIRModule(HIRValue):
     def __init__(self, context: HIRContext) -> None:
         super().__init__(context)
         self.entryPoint: HIRValue = None
-        self.functionalDefinitions = []
+        self.globalValues: list[HIRGlobalValue] = []
 
     def getType(self):
         return None
+    
+    def addGlobalValue(self, globalValue: HIRGlobalValue):
+        self.globalValues.append(globalValue)
+
+    def enumerateGlobalValues(self):
+        index = 0
+        for globalValue in self.globalValues:
+            globalValue.globalIndex = index
+            index += 1
 
     def prettyPrint(self) -> str:
+        self.enumerateGlobalValues()
+
         result = ''
         if self.entryPoint is not None:
             result += 'entryPoint: %s\n' % str(self.entryPoint)
 
-        for functionalDefinition in self.functionalDefinitions:
-            result += functionalDefinition.fullPrintString()
+        for globalValue in self.globalValues:
+            result += globalValue.fullPrintString()
         return result
 
 class HIRModuleFrontend:
@@ -69,12 +236,36 @@ class HIRModuleFrontend:
         self.context = HIRContext()
         self.module = HIRModule(self.context)
         self.translatedValueDictionary = dict()
+        self.translatedConstantValueDictionary = dict()
+        
+
+        for baseType, targetType in [
+            [UnitType, self.context.unitType],
+            [Int8Type, self.context.int8Type],
+            [Int16Type, self.context.int16Type],
+            [Int32Type, self.context.int32Type],
+            [Int64Type, self.context.int64Type],
+            [Float32Type, self.context.float32Type],
+            [Float64Type, self.context.float64Type],
+            [FalseType, self.context.booleanType],
+            [TrueType, self.context.booleanType],
+            [BooleanType, self.context.booleanType],
+        ]:
+            self.translatedConstantValueDictionary[baseType] = targetType
 
     def compileGraphModule(self, graphModule: GHIRModule) -> HIRModule:
         if graphModule.entryPoint is not None:
-            self.entryPoint = self.translateGraphValue(graphModule.entryPoint)
+            self.module.entryPoint = self.translateGraphValue(graphModule.entryPoint)
         return self.module
-    
+
+    def translateConstantTypedValue(self, typedValue: TypedValue) -> HIRValue:
+        if typedValue in self.translatedConstantValueDictionary:
+            return self.translatedConstantValueDictionary[typedValue]
+        
+        translatedValue = typedValue.acceptTypedValueVisitor(self)
+        self.translatedConstantValueDictionary[typedValue] = translatedValue
+        return translatedValue
+
     def translateGraphValue(self, graphValue: GHIRValue) -> HIRValue:
         if graphValue in self.translatedValueDictionary:
             return self.translatedValueDictionary[graphValue]
@@ -83,13 +274,24 @@ class HIRModuleFrontend:
         self.translatedValueDictionary[graphValue] = translatedValue
         return translatedValue
     
+    def visitConstantValue(self, graphValue: GHIRConstantValue) -> HIRValue:
+        return self.translateConstantTypedValue(graphValue.value)
+    
     def visitLambdaValue(self, graphValue: GHIRLambdaValue) -> HIRValue:
+        lambdaType = self.translateGraphValue(graphValue.type)
         definition = self.translateGraphValue(graphValue.definition)
         captures = list(map(self.translateGraphValue, graphValue.captures))
-        return HIRConstantLambda(self.context, None, captures, definition)
+        return HIRConstantLambda(self.context, lambdaType, captures, definition)
 
     def visitFunctionalDefinitionValue(self, graphValue: GHIRFunctionalDefinitionValue) -> HIRValue:
         hirDefinition = HIRFunctionalDefinition(self.context, None)
         self.translatedValueDictionary[graphValue] = hirDefinition
-        self.module.functionalDefinitions.append(hirDefinition)
+        self.module.addGlobalValue(hirDefinition)
         return hirDefinition
+    
+    def visitSimpleFunctionType(self, graphValue: GHIRSimpleFunctionType) -> HIRValue:
+        hirFunctionType = HIRFunctionType(self.context)
+        self.translatedValueDictionary[graphValue] = hirFunctionType
+        hirFunctionType.argumentTypes = list(map(self.translateGraphValue, graphValue.arguments))
+        hirFunctionType.resultType = self.translateGraphValue(graphValue.resultType)
+        return hirFunctionType
